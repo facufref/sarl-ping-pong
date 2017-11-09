b6d7a12e4bf355ac670d69564e7560e24ba82c1e
Nahuel Diaz Lederhos <nahuel_dl94@hotmail.com>
2017-11-09 18:33:45 -0300

    Initial commit

---
+++ src/main/generated-sources/sarl/Ping.java
@@ -0,0 +1,56 @@
+import io.sarl.lang.annotation.SarlElementType;
+import io.sarl.lang.annotation.SarlSpecification;
+import io.sarl.lang.annotation.SyntheticMember;
+import io.sarl.lang.core.Event;
+import org.eclipse.xtext.xbase.lib.Pure;
+
+@SarlSpecification("0.5")
+@SarlElementType(13)
+@SuppressWarnings("all")
+public class Ping extends Event {
+  public int index;
+  
+  public Ping(final int i) {
+    this.index = i;
+  }
+  
+  @Override
+  @Pure
+  @SyntheticMember
+  public boolean equals(final Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Ping other = (Ping) obj;
+    if (other.index != this.index)
+      return false;
+    return super.equals(obj);
+  }
+  
+  @Override
+  @Pure
+  @SyntheticMember
+  public int hashCode() {
+    int result = super.hashCode();
+    final int prime = 31;
+    result = prime * result + this.index;
+    return result;
+  }
+  
+  /**
+   * Returns a String representation of the Ping event's attributes only.
+   */
+  @SyntheticMember
+  @Pure
+  protected String attributesToString() {
+    StringBuilder result = new StringBuilder(super.attributesToString());
+    result.append("index  = ").append(this.index);
+    return result.toString();
+  }
+  
+  @SyntheticMember
+  private final static long serialVersionUID = 692059479L;
+}
---
+++ src/main/generated-sources/sarl/PongAgent.java
@@ -0,0 +1,74 @@
+import com.google.common.base.Objects;
+import io.sarl.core.DefaultContextInteractions;
+import io.sarl.lang.annotation.ImportedCapacityFeature;
+import io.sarl.lang.annotation.PerceptGuardEvaluator;
+import io.sarl.lang.annotation.SarlElementType;
+import io.sarl.lang.annotation.SarlSpecification;
+import io.sarl.lang.annotation.SyntheticMember;
+import io.sarl.lang.core.Address;
+import io.sarl.lang.core.Agent;
+import io.sarl.lang.core.BuiltinCapacitiesProvider;
+import io.sarl.lang.core.Scope;
+import io.sarl.lang.core.Skill;
+import io.sarl.lang.util.ClearableReference;
+import java.util.Collection;
+import java.util.UUID;
+import javax.inject.Inject;
+import org.eclipse.xtext.xbase.lib.Extension;
+import org.eclipse.xtext.xbase.lib.Inline;
+import org.eclipse.xtext.xbase.lib.InputOutput;
+import org.eclipse.xtext.xbase.lib.Pure;
+
+@SarlSpecification("0.5")
+@SarlElementType(16)
+@SuppressWarnings("all")
+public class PongAgent extends Agent {
+  @SyntheticMember
+  private void $behaviorUnit$Ping$0(final Ping occurrence) {
+    int indexReceived = occurrence.index;
+    if ((indexReceived < 100)) {
+      InputOutput.<String>println(("Ping " + Integer.valueOf(indexReceived)));
+      DefaultContextInteractions _$CAPACITY_USE$IO_SARL_CORE_DEFAULTCONTEXTINTERACTIONS$CALLER = this.$castSkill(DefaultContextInteractions.class, (this.$CAPACITY_USE$IO_SARL_CORE_DEFAULTCONTEXTINTERACTIONS == null || this.$CAPACITY_USE$IO_SARL_CORE_DEFAULTCONTEXTINTERACTIONS.get() == null) ? (this.$CAPACITY_USE$IO_SARL_CORE_DEFAULTCONTEXTINTERACTIONS = this.$getSkill(DefaultContextInteractions.class)) : this.$CAPACITY_USE$IO_SARL_CORE_DEFAULTCONTEXTINTERACTIONS);
+      Pong _pong = new Pong((indexReceived + 1));
+      final Scope<Address> _function = (Address it) -> {
+        Address _source = occurrence.getSource();
+        return Objects.equal(it, _source);
+      };
+      _$CAPACITY_USE$IO_SARL_CORE_DEFAULTCONTEXTINTERACTIONS$CALLER.emit(_pong, _function);
+    }
+  }
+  
+  @Extension
+  @ImportedCapacityFeature(DefaultContextInteractions.class)
+  @SyntheticMember
+  private transient ClearableReference<Skill> $CAPACITY_USE$IO_SARL_CORE_DEFAULTCONTEXTINTERACTIONS;
+  
+  @SyntheticMember
+  @Pure
+  @Inline(value = "$castSkill(DefaultContextInteractions.class, ($0$CAPACITY_USE$IO_SARL_CORE_DEFAULTCONTEXTINTERACTIONS == null || $0$CAPACITY_USE$IO_SARL_CORE_DEFAULTCONTEXTINTERACTIONS.get() == null) ? ($0$CAPACITY_USE$IO_SARL_CORE_DEFAULTCONTEXTINTERACTIONS = $0$getSkill(DefaultContextInteractions.class)) : $0$CAPACITY_USE$IO_SARL_CORE_DEFAULTCONTEXTINTERACTIONS)", imported = DefaultContextInteractions.class)
+  private DefaultContextInteractions $CAPACITY_USE$IO_SARL_CORE_DEFAULTCONTEXTINTERACTIONS$CALLER() {
+    if (this.$CAPACITY_USE$IO_SARL_CORE_DEFAULTCONTEXTINTERACTIONS == null || this.$CAPACITY_USE$IO_SARL_CORE_DEFAULTCONTEXTINTERACTIONS.get() == null) {
+      this.$CAPACITY_USE$IO_SARL_CORE_DEFAULTCONTEXTINTERACTIONS = $getSkill(DefaultContextInteractions.class);
+    }
+    return $castSkill(DefaultContextInteractions.class, this.$CAPACITY_USE$IO_SARL_CORE_DEFAULTCONTEXTINTERACTIONS);
+  }
+  
+  @SyntheticMember
+  @PerceptGuardEvaluator
+  private void $guardEvaluator$Ping(final Ping occurrence, final Collection<Runnable> ___SARLlocal_runnableCollection) {
+    assert occurrence != null;
+    assert ___SARLlocal_runnableCollection != null;
+    ___SARLlocal_runnableCollection.add(() -> $behaviorUnit$Ping$0(occurrence));
+  }
+  
+  @SyntheticMember
+  public PongAgent(final UUID parentID, final UUID agentID) {
+    super(parentID, agentID);
+  }
+  
+  @SyntheticMember
+  @Inject
+  public PongAgent(final BuiltinCapacitiesProvider provider, final UUID parentID, final UUID agentID) {
+    super(provider, parentID, agentID);
+  }
+}
